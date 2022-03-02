26a19b4cdff9b2170695cdf7a495050bf4b1e0d7
radovanovicslobodan <r.slobodan75@gmail.com>
2022-03-03 00:05:44 +0100

    add WebDriverWaitProvider

---
+++ src/test/java/cucumber_blueprint/core/driver/WebDriverWaitProvider.java
@@ -0,0 +1,19 @@
+package cucumber_blueprint.core.driver;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WebDriverWaitProvider implements Provider<WebDriverWait> {
+
+    @Inject
+    WebDriver driver;
+
+    @Override
+    public WebDriverWait get() {
+        return new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+}
--- src/test/java/cucumber_blueprint/core/guice/AppModule.java
+++ src/test/java/cucumber_blueprint/core/guice/AppModule.java
@@ -2,13 +2,16 @@ package cucumber_blueprint.core.guice;
 
 import com.google.inject.AbstractModule;
 import cucumber_blueprint.core.driver.SharedDriver;
+import cucumber_blueprint.core.driver.WebDriverWaitProvider;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AppModule extends AbstractModule {
 
     @Override
     protected void configure() {
         bind(WebDriver.class).to(SharedDriver.class);
+        bind(WebDriverWait.class).toProvider(WebDriverWaitProvider.class);
     }
 }
 
