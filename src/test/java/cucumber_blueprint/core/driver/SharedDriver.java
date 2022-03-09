41aaaa773984eb48ef93246044474b0210ca8252
radovanovicslobodan <r.slobodan75@gmail.com>
2022-03-09 23:05:35 +0100

    refactor SharedDriver

--- src/test/java/cucumber_blueprint/core/driver/SharedDriver.java
+++ src/test/java/cucumber_blueprint/core/driver/SharedDriver.java
@@ -8,15 +8,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.time.Duration;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
+import static cucumber_blueprint.core.driver.helpers.DriverOptions.chromeOptions;
+import static cucumber_blueprint.core.driver.helpers.DriverOptions.firefoxOptions;
 import static cucumber_blueprint.utils.ConfigUtils.getProp;
 
 @ScenarioScoped
@@ -27,7 +26,6 @@ public class SharedDriver implements WebDriver {
     private WebDriver getDelegate() {
 
         if (delegate == null) {
-            System.out.println("Creating lazy initialization...");
 
             switch (getProp(Props.DRIVER_TYPE.prop)) {
                 case "headlessChrome":
@@ -58,8 +56,6 @@ public class SharedDriver implements WebDriver {
                     throw new IllegalStateException("Unexpected value: " + getProp(Props.DRIVER_TYPE.prop));
             }
         }
-
-        System.out.println("DELEGATE " + delegate);
         return delegate;
     }
 
@@ -134,34 +130,5 @@ public class SharedDriver implements WebDriver {
     @Override
     public WebDriver.Options manage() {
         return getDelegate().manage();
-    }
-
-    public static ChromeOptions chromeOptions(boolean headless) {
-        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-        chromePrefs.put("profile.default_content_settings.popups", 0);
-        chromePrefs.put("download.default_directory", System.getProperty("user.dir") + "\\src\\test\\resources\\downloaded_files");
-
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setHeadless(headless);
-        chromeOptions.addArguments("start-maximized");
-        chromeOptions.addArguments("--disable-extensions");
-        chromeOptions.addArguments("--disable-infobars");
-        chromeOptions.addArguments("--ignore-certificate-errors");
-        chromeOptions.setExperimentalOption("prefs", chromePrefs);
-        //     chromeOptions.addArguments("window-size=1920,1080");
-        return chromeOptions;
-    }
-
-    public static FirefoxOptions firefoxOptions(boolean headless) {
-        FirefoxOptions firefoxOptions = new FirefoxOptions();
-//        firefoxOptions.addArguments("start-maximized");
-        firefoxOptions.addArguments("--disable-extensions");
-        firefoxOptions.addArguments("--ignore-certificate-errors");
-        firefoxOptions.addArguments("--disable-infobars");
-        firefoxOptions.addArguments("--disable-gpu");
-        firefoxOptions.setHeadless(headless);
-        // FIXME: headless???
-        firefoxOptions.setAcceptInsecureCerts(headless);
-        return firefoxOptions;
     }
 }
---
+++ src/test/java/cucumber_blueprint/core/driver/helpers/DriverOptions.java
@@ -0,0 +1,38 @@
+package cucumber_blueprint.core.driver.helpers;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.util.HashMap;
+
+public class DriverOptions {
+
+    public static ChromeOptions chromeOptions(boolean headless) {
+        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+//        chromePrefs.put("download.default_directory", System.getProperty("user.dir") + "\\src\\test\\resources\\downloaded_files");
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setHeadless(headless);
+        chromeOptions.addArguments("start-maximized");
+        chromeOptions.addArguments("--disable-extensions");
+        chromeOptions.addArguments("--disable-infobars");
+        chromeOptions.addArguments("--ignore-certificate-errors");
+        chromeOptions.setExperimentalOption("prefs", chromePrefs);
+//        chromeOptions.addArguments("window-size=1920,1080");
+        return chromeOptions;
+    }
+
+    public static FirefoxOptions firefoxOptions(boolean headless) {
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+//        firefoxOptions.addArguments("start-maximized");
+        firefoxOptions.addArguments("--disable-extensions");
+        firefoxOptions.addArguments("--ignore-certificate-errors");
+        firefoxOptions.addArguments("--disable-infobars");
+        firefoxOptions.addArguments("--disable-gpu");
+        firefoxOptions.setHeadless(headless);
+        // FIXME: headless???
+        firefoxOptions.setAcceptInsecureCerts(headless);
+        return firefoxOptions;
+    }
+}
