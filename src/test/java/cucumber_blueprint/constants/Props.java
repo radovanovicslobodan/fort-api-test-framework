328b3d032d219cc305eb55a93423f017ff9a423c
Slobodan <slobodan.radovanovic@htecgroup.com>
2022-03-14 16:58:45 +0100

    delete unnecessary comments

--- src/test/java/cucumber_blueprint/constants/Props.java
+++ src/test/java/cucumber_blueprint/constants/Props.java
@@ -2,9 +2,6 @@ package cucumber_blueprint.constants;
 
 public class Props {
 
-//    DRIVER_TYPE("driverType"),
-//    BASE_REST_URI("baseRestUri"),
-//    BASE_WEB_URI("baseWebUri");
     public static final String DRIVER_TYPE="driverType";
     public static final String BASE_REST_URI="baseRestUri";
     public static final String BASE_WEB_URI="baseWebUri";
