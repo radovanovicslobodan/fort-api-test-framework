505b05377cfb65de322ffb37ceea175fa89a36fb
Slobodan <slobodan.radovanovic@htecgroup.com>
2022-03-14 16:59:34 +0100

    formatting Props.java

--- src/test/java/cucumber_blueprint/constants/Props.java
+++ src/test/java/cucumber_blueprint/constants/Props.java
@@ -2,8 +2,8 @@ package cucumber_blueprint.constants;
 
 public class Props {
 
-    public static final String DRIVER_TYPE="driverType";
-    public static final String BASE_REST_URI="baseRestUri";
-    public static final String BASE_WEB_URI="baseWebUri";
+    public static final String DRIVER_TYPE = "driverType";
+    public static final String BASE_REST_URI = "baseRestUri";
+    public static final String BASE_WEB_URI = "baseWebUri";
 
 }
