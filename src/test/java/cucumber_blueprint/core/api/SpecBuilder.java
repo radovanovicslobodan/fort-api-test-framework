e3e4f433df522a63eaa50df7d12d6a0178540f77
Slobodan <slobodan.radovanovic@htecgroup.com>
2022-03-15 13:52:24 +0100

    create SpecBuilder and refactor AuthHelpers

--- src/test/java/cucumber_blueprint/core/api/SpecBuilder.java
+++ src/test/java/cucumber_blueprint/core/api/SpecBuilder.java
@@ -1,4 +1,18 @@
 package cucumber_blueprint.core.api;
 
+import cucumber_blueprint.constants.Props;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+
+import static cucumber_blueprint.utils.ConfigUtils.getProp;
+
 public class SpecBuilder {
+
+    public static RequestSpecBuilder requestSpecBuilder() {
+        RequestSpecBuilder builder = new RequestSpecBuilder();
+
+        builder.setBaseUri(getProp(Props.BASE_REST_URI));
+        builder.setContentType(ContentType.JSON);
+        return builder;
+    }
 }
--- src/test/java/cucumber_blueprint/rest/helpers/AuthHelpers.java
+++ src/test/java/cucumber_blueprint/rest/helpers/AuthHelpers.java
@@ -2,15 +2,11 @@ package cucumber_blueprint.rest.helpers;
 
 import cucumber_blueprint.constants.HttpMethod;
 import cucumber_blueprint.constants.Paths;
-import cucumber_blueprint.constants.Props;
+import cucumber_blueprint.core.api.SpecBuilder;
 import cucumber_blueprint.rest.pojos.AuthBody;
 import cucumber_blueprint.utils.ApiUtils;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-
-import static cucumber_blueprint.utils.ConfigUtils.getProp;
 
 public class AuthHelpers {
 
@@ -18,14 +14,11 @@ public class AuthHelpers {
 
         AuthBody body = new AuthBody(username, password);
 
-        RequestSpecBuilder builder = new RequestSpecBuilder();
-
-        // Prepare request
-        builder.setBaseUri(getProp(Props.BASE_REST_URI));
-        builder.setBasePath(Paths.AUTH);
-        builder.setContentType(ContentType.JSON);
-        builder.setBody(body);
-        RequestSpecification requestSpec = builder.build();
+        RequestSpecification requestSpec = SpecBuilder
+                .requestSpecBuilder()
+                .setBasePath(Paths.AUTH)
+                .setBody(body)
+                .build();
 
         return ApiUtils.sendRequest(requestSpec, HttpMethod.POST);
     }
